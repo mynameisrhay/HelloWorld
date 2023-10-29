798484656b1626ae96de58597cbcfc3aa87fe590
Raymond Bautista <mynameisrhay@gmail.com>
2023-10-29 16:50:00 +0800

    added code in basic_print.py

---
+++ basic_print.py
@@ -0,0 +1,6 @@
+#first exercise. need to ask person's name and color, then print a message saying like John likes blue.
+
+username = input("What is your name? ")
+favorite_color = input("How about your favorite color? ")
+
+print(f"{username} likes {favorite_color}")
