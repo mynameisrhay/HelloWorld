ed9ebf8327c7a36e3f62335f33ad4d060af49258
Raymond Bautista <mynameisrhay@gmail.com>
2023-10-29 17:07:20 +0800

    minor changes on basic_print.py and added code for weight_converter.py

--- weight_converter.py
+++ weight_converter.py
@@ -1,4 +1,11 @@
-#2nd exercise. simple weight converter from lbs to kgs vice versa
+# 2nd exercise. simple weight converter from lbs to kgs vice versa. this won't use the IF function
 
-weight = input("What is your weight? ")
-input("Is it it ")
+weight = float(input("What is your weight? "))
+weight_unit = input("Is it in (K)gs or (L)bs? ")
+
+if weight_unit.upper() == "K":
+    print(f"Your weight in Lbs is {weight * 2.2}")
+elif weight_unit.upper() == "L":
+    print(f"Your weight in Kgs is {weight / 2.2}")
+else:
+    print("Specify your weight unit correctly!")
