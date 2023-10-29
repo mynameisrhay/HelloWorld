e7b2528f420e9cbc8e426aa8976079c0a658f1d1
Raymond Bautista <mynameisrhay@gmail.com>
2023-10-29 18:33:12 +0800

    created name_length_checker.py and added note

---
+++ name_length_checker.py
@@ -0,0 +1,2 @@
+# 4th exercise. name validator. if name is less than 3 characters, prompt the user that it must be at least 3 characters. If more than 50 characters, prompt user that it must be a maximum of 50 characters.
+
