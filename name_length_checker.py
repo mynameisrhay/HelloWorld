53094573987b72e1d6436ce321349a83100f7ec4
Raymond Bautista <mynameisrhay@gmail.com>
2023-10-29 18:42:51 +0800

    added code for name_length_checker.py. added printq live template on pycharm, just a reference.

--- name_length_checker.py
+++ name_length_checker.py
@@ -1,2 +1,10 @@
 # 4th exercise. name validator. if name is less than 3 characters, prompt the user that it must be at least 3 characters. If more than 50 characters, prompt user that it must be a maximum of 50 characters.
 
+username = input("Name: ")
+
+if len(username) < 3:
+    print("Username must be at least 3 characters")
+elif len(username) > 50:
+    print("Username must be a maximum of 50 characters")
+else:
+    print("Username looks good!")
