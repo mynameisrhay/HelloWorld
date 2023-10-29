37337bc8bafe040899ad57a07a2cea43d5bb2016
Raymond Bautista <mynameisrhay@gmail.com>
2023-10-29 19:23:17 +0800

    7th exercise. created forloop_total.py. this calculates the total of a list using for loop. very basic exercise of for loop.

--- app.py
+++ app.py
@@ -1,6 +1,2 @@
-numbers = [2, 2, 4, 3, 4, 1, 7, 9]
-uniques = []
-for number in numbers:
-    if number not in uniques:
-        uniques.append(number)
-print(uniques)
+for item in "Python":
+    print(item)
---
+++ forloop_total.py
@@ -0,0 +1,7 @@
+# 7th exercise. calculate the total of a shopping cart (list) using for loop
+
+cart = [10, 30, 44.50]
+total = 0
+for item in cart:
+    total += item
+print(total)
