f0d87a6c0e6c427321b8d5a08966a3e5debc8d90
Raymond Bautista <mynameisrhay@gmail.com>
2023-10-29 19:16:01 +0800

    leveled up the car_game.py by adding a functionality that stops the user from starting the car if it has already started. same goes with stopping it.

--- car_game.py
+++ car_game.py
@@ -1,6 +1,8 @@
 # 6th exercise. simple car game that has 4 functions, start, stop, help, and quit. must exercise while loop
 # and if functions effectively.
 
+car_started = False
+
 while True:
     command = input("> ").lower()
     if command == "help":
@@ -8,11 +10,19 @@ while True:
 stop - to stop the car
 quit - to end the application""")
     elif command == "start":
-        print("Car has started")
+        if not car_started:
+            print("Car has started")
+            car_started = True
+        else:
+            print("Car has already started.")
     elif command == "stop":
-        print("Car has stopped")
+        if car_started:
+            print("Car has stopped")
+            car_started = False
+        else:
+            print("Car has already stopped.")
     elif command == "quit":
         print("Thanks for playing!")
         break
     else:
-        print("Sorry, I don't understand that")
+        print("Sorry, I don't understand that. Type 'help' for the available commands.")
