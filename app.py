7029e780f766eab147ed441b25f9786428d9fb40
Raymond Bautista <mynameisrhay@gmail.com>
2023-11-02 22:03:20 +0800

    practiced using the return statement

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 def greet_user(first_name, last_name):
     print(f"Hello {first_name} {last_name}!")
+    return first_name + " " + last_name
 
 
-greet_user("John", "Smith")
+full_name = ""
+full_name = greet_user("John", "Smith")
+print(f"Full name based on greet_user function is {full_name}")
