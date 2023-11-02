cb2519d31c8fa12c62e68459bacff7d3ab5468c7
Raymond Bautista <mynameisrhay@gmail.com>
2023-11-02 22:17:32 +0800

    done with 12th exercise. made a reusable function using the code from the 11th exercise that is number to words converter.

--- using_function.py
+++ using_function.py
@@ -1,2 +1,22 @@
 # 12th exercise. need to use the code number to words converter and make it a reusable function
 
+def num_converter(number):
+    value = ""
+    phone = {
+        "1": "One",
+        "2": "Two",
+        "3": "Three",
+        "4": "Four",
+        "5": "Five",
+        "6": "Six",
+        "7": "Seven",
+        "8": "Eight",
+        "9": "Nine",
+    }
+    for char in number:
+        value += phone.get(char, "*") + " "
+    return value
+
+
+ch = input("Phone: ")
+print(num_converter(ch))
