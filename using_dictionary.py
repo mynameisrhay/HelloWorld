a27ec8cc76074af1553a305a3a0602ae1a5e5cc3
Raymond Bautista <mynameisrhay@gmail.com>
2023-11-02 21:34:32 +0800

    completed 11th exercise. the program will convert number to words, letters will be converted into asterisk. fun exercise

--- using_dictionary.py
+++ using_dictionary.py
@@ -1,2 +1,19 @@
 # 11th exercise. using dictionary, make a program where the phone number will be translated from numbers to words
 
+phone = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+}
+
+number = input("Phone: ")
+value = ""
+for char in number:
+    value += phone.get(char, "*") + " "
+print(value)
