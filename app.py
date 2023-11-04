b55c63bce96f3875db56c5737ec7ec237da8ebe9
Raymond Bautista <mynameisrhay@gmail.com>
2023-11-04 13:30:38 +0800

    done with practicing the use of python packages and module. used 3 approach, import only, from package.module import function, and from package import module. depending on use case, I can apply different methods to call functions under packages. deliberately didn't use 'as' for this practice.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-# practice using modules
+# practice using packages. will not practice using "as" at this point of practice.
 
-import converters
+# 1st approach, import the module under python package by using only import. this will make the code longer.
+import ecommerce.shipping
 
-print(converters.kg_to_lbs(75))
-print(converters.lbs_to_kg(165))
+ecommerce.shipping.calc_total()
+ecommerce.shipping.calc_total()
 
-# from converters import kg_to_lbs
-#
-# print(kg_to_lbs(75))
+# 2nd approach, use from and call package and module then import function. a bit shorter
+from ecommerce.shipping import calc_total
+
+calc_total()
+calc_total()
+
+# 3rd approach, use from and call package, then import module
+from ecommerce import shipping
+
+shipping.calc_total()
+shipping.calc_total()
---
+++ ecommerce/shipping.py
@@ -0,0 +1,2 @@
+def calc_total():
+    print("Calculated total")
